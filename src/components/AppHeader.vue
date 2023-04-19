691e8569f5c6d253b5e1249c59d26207e3d6db66
Cassovich <cassws00@gmail.com>
2023-04-19 14:42:39 +0200

    div'er sat op

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,36 @@
 <template>
-  <header></header>
+  <header>
+    <div class="header-container">
+      <div>
+        <p>Team Toaster</p>
+        <p>(Kunde nr.:4242)</p>
+      </div>
+
+      <div>
+        <p>344 dage tilbage i prøveperiode med alle funktioner tilgængelige Vælg abonnement</p>
+      </div>
+
+      <div class="tredje-div">
+        <div>klokke</div> 
+        <div>
+          <div>person</div>
+          <div><p>CuriousDeveloperTestingStuff</p></div>
+          <div>pil</div>
+        </div>
+      </div>
+
+    </div>
+  </header>
 </template>
 
 <style lang="scss">
   header {
     background-color: #eee;
   }
+
+  .header-container{
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+
 </style>
