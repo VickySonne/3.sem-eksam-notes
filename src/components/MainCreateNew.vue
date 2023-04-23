4d118b57db4fe1d6aa59cefbd8b610a3a6bd0965
VickySonne <vst1996@live.dk>
2023-04-23 14:30:11 +0200

    style create new task

--- src/components/MainCreateNew.vue
+++ src/components/MainCreateNew.vue
@@ -3,122 +3,303 @@ import TodoCatagoriDropdownComp from './TodoCatagoriDropdownComp.vue';
 </script>
 
 <template>
-
-    <div>
-        <i>i</i>
-        <p>Tilbage til sagsstyrring</p>
-    </div>
-
-    <div>
-
-        <section class="customer">
-            <h3>Kunde</h3>
-            <div>
-                <div class="search-field">
-                    <i>i</i>
-                    <input type="search" placeholder="Find kunde...">
-                </div>
-                <p>Opret Ny Kunde</p>
-                <div>
-                    <div>
-                        <p>Kunde Navn <span>Kundegruppe</span></p>
-                        <p>Telefon nummer</p>
-                    </div>
-                    <div>
-                        <div>
-                            <p>Vejnavn</p>
-                            <p><span>Postnummer</span>&nbsp;<span>Bynavn</span></p>
-                        </div>
-                        <div>
-                            <i>x</i>
-                        </div>
-                    </div>
-                </div>
+    <div class="main-layout">
+        <div class="main-left-content">
+            <div class="back-btn">
+                <i>i</i>
+                <p>Tilbage til sagsstyrring</p>
             </div>
-        </section>
 
-        <section class="todos">
-            <h3>Opgaver</h3>
-            <div>
-                <div>
-                    <TodoCatagoriDropdownComp />
-                    <TodoCatagoriDropdownComp />
-                    <TodoCatagoriDropdownComp />
-                </div>
-                <div class="create-custom-todo">
-                    <i>i</i>
-                    <p>Custom Opgave</p>
-                </div>
+            <div class="main-content">
+
+                <section class="customer">
+                    <h3>Kunde</h3>
+                    <div class="search-bar">
+                        <div class="search-field">
+                            <i>i</i>
+                            <input type="search" placeholder="Find kunde...">
+                        </div>
+                        <p>Opret Ny Kunde</p>
+                        <div class="selected-customer">
+                            <div>
+                                <p>Kunde Navn <span>Kundegruppe</span></p>
+                                <p>Telefon nummer</p>
+                            </div>
+                            <div>
+                                <div>
+                                    <p>Vejnavn</p>
+                                    <p><span>Postnummer</span>&nbsp;<span>Bynavn</span></p>
+                                </div>
+                                <div>
+                                    <i>x</i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <section class="todos">
+                    <h3>Opgaver</h3>
+                    <div class="todo-catagories-list section-bg">
+                        <div class="catagories">
+                            <TodoCatagoriDropdownComp />
+                            <TodoCatagoriDropdownComp />
+                            <TodoCatagoriDropdownComp />
+                        </div>
+                        <div class="create-custom-todo">
+                            <i>i</i>
+                            <p>Custom Opgave</p>
+                        </div>
+                    </div>
+                </section>
+
+                <section class="details">
+                    <h3>Detaljer</h3>
+
+                    <form class="grid-wrapper section-bg">
+                        <div class="form-input">
+                            <label for="">Ansvarlig</label>
+                            <div class="input-field">
+                                <p>Name</p>
+                                <i>i</i>
+                            </div>
+                        </div>
+
+                        <div class="form-input">
+                            <label for="">Status</label>
+                            <div class="input-field">
+                                <p>Status</p>
+                                <i>i</i>
+                            </div>
+                        </div>
+
+                        <div class="form-input">
+                            <label for="">Tags</label>
+                            <div class="input-field">
+                                <p>Tag</p>
+                                <i>i</i>
+                            </div>
+                        </div>
+
+                        <div class="flex-wrapper">
+                            <div class="form-input">
+                                <label for="">Afhentning</label>
+                                <input class="input-field" type="datetime-local">
+                            </div>
+                            <div class="form-input">
+                                <label for="">Aftalt Pris</label>
+                                <div class="price-input">
+                                    <input class="input-field" type="number" placeholder="Intern pris">
+                                    <p>kr.</p>
+                                </div>
+                                
+                            </div>
+                        </div>
+
+                        <div class="form-input description">
+                            <label for="">Beskrivelse</label>
+                            <textarea class="input-field" name="" id="" cols="30" rows="10"></textarea>
+                        </div>
+                    </form>
+                </section>
+
+                <section class="overflow">
+                    Indsæt Vare component her
+                </section>
+
             </div>
-        </section>
-
-        <section class="details">
-            <h3>Detaljer</h3>
-
-            <form>
-                <div>
-                    <label for="">Ansvarlig</label>
-                    <div>
-                        <p>Name</p>
-                        <i>i</i>
-                    </div>
-                </div>
-
-                <div>
-                    <label for="">Status</label>
-                    <div>
-                        <p>Status</p>
-                        <i>i</i>
-                    </div>
-                </div>
-
-                <div>
-                    <label for="">Tags</label>
-                    <div>
-                        <p>Tag</p>
-                        <i>i</i>
-                    </div>
-                </div>
-
-                <div>
-                    <div>
-                        <label for="">Afhentning</label>
-                        <input type="datetime-local">
-                    </div>
-                    <div>
-                        <label for="">Aftalt Pris</label>
-                        <input type="number" placeholder="kr.">
-                    </div>
-                </div>
-
-                <div>
-                    <label for="">Beskrivelse</label>
-                    <textarea name="" id="" cols="30" rows="10"></textarea>
-                </div>
-            </form>
-        </section>
-
-        <section>
-            Indsæt Vare component her
-        </section>
-
-    </div>
-
-    <aside>
-        
-        <div>
-            <h3>Opsummering</h3>
-        </div>
-        
-        <div class="create-task">
-            <i>i</i>
-            <p>Opret Sag</p>
         </div>
 
-    </aside>
+        <aside>
+            
+            <div class="section-bg">
+                <h3>Opsummering</h3>
+            </div>
+            
+            <div class="create-task">
+                <i>i</i>
+                <p>Opret Sag</p>
+            </div>
+
+        </aside>
+    </div>
 
 </template>
 
 <style lang="scss" scoped>
 
+    .main-layout{
+        display: flex;
+        flex-direction: row;
+
+        .main-left-content{
+            width: 70%;
+        }
+
+        aside{
+            width: 100%;
+            max-width: 30%;
+            position: sticky;
+            top: 0;
+        }
+    }
+    .main-content{
+        display: flex;
+        flex-direction: column;
+        gap: 3rem ;
+    }
+
+    h3{
+        font-weight: 700;
+        font-size: 1.125rem;
+        margin-bottom: 1rem;
+    }
+    .back-btn{
+        display: flex;
+        flex-direction: row;
+        gap: 1rem;
+        margin-bottom: 1rem;
+        cursor: pointer;
+        font-weight: 700;
+
+        // p{
+        //     font-weight: 500;
+        // }
+    }
+
+    .search-bar{
+        display: flex;
+        flex-direction: row;
+        gap: 1rem;
+        align-items: center;
+
+        & > p{
+                font-weight: 700;
+                cursor: pointer;
+        }
+
+        & > p:hover{
+            text-decoration: underline;
+        }
+    }
+
+    .selected-customer{
+        display: none;
+    }
+
+    .search-field{
+            background-color: rgb(245 245 245);
+            padding: 1rem 3rem;
+            position: relative;
+
+            i{
+                position: absolute;
+                left: 1rem;
+                pointer-events: none;
+            }
+
+            input{
+                cursor: text;
+            }
+
+            ::placeholder {
+                color: grey;
+                opacity: 1; /* Firefox */
+            }
+
+            :-ms-input-placeholder { /* Internet Explorer 10-11 */
+                color: grey;
+            }
+
+            ::-ms-input-placeholder { /* Microsoft Edge */
+                color: grey;
+            }
+        }
+
+        .section-bg{
+            padding: 1rem;
+            background-color: rgb(209 213 219);
+            border-radius: 0.125rem;
+        }
+
+        .todo-catagories-list{
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+
+            .catagories{
+                display: flex;
+                gap: 1rem;
+            }
+        }
+
+        .create-custom-todo{
+            display: flex;
+            flex-direction: row;
+            gap: 1rem;
+            background-color: white;
+            padding: 1rem;
+            border-radius: 0.125rem;
+            cursor: pointer;
+        }
+
+        .grid-wrapper{
+            display: grid;
+            grid-template-columns: repeat(2,minmax(0,1fr));
+            gap: 1rem;
+
+            .form-input{
+                display: flex;
+                flex-direction: column;
+                gap: 0.5rem;
+
+                label{
+                    font-weight: 600;
+                }
+
+                .input-field{
+                    display: flex;
+                    flex-direction: row;
+                    justify-content: space-between;
+                    background-color: white;
+                    padding: 0.5rem;
+                    border: 0 solid #e5e7eb;
+                    border-radius: 0.125rem;
+                }
+            }
+
+            .description{
+                grid-column: span 2;
+            }
+        }
+
+        .flex-wrapper{
+            display: flex;
+            flex-direction: row;
+            gap: 1rem;
+
+            & > div{
+                width: 100%;
+            }
+        }
+
+        .price-input{
+            position: relative;
+
+            input{
+                width: 100%;
+            }
+
+            p{
+                position: absolute;
+                right: 0.5rem;
+                bottom: 0.5rem;
+                
+            }   
+        }
+
+        .overflow{
+            height: 750px;
+            background-color: red;
+        }
 </style>
--- src/components/TodoCatagoriDropdownComp.vue
+++ src/components/TodoCatagoriDropdownComp.vue
@@ -3,18 +3,39 @@
 </script>
 
 <template>
-    <div>
-        <h4>Kategori Name</h4>
-        <p>#</p>
-        <ul>
-            <li>Task 1</li>
-            <li>Task 2</li>
-            <li>Task 3</li>
-            <li>Task 4</li>
-            <li>Task 5</li>
-        </ul>
+    <div class="catagori">
+        <div>
+            <h4>Kategori Name</h4>
+            <ul>
+                <li>Task 1</li>
+                <li>Task 2</li>
+                <li>Task 3</li>
+                <li>Task 4</li>
+                <li>Task 5</li>
+            </ul>
+        </div>
+        
+        <i>i</i>
     </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
+
+    .catagori{
+        display: flex;
+        flex-direction: row;
+        gap: 1rem;
+        background-color: white;
+        padding: 1rem;
+        border-radius: 0.125rem;
+
+        i{
+            cursor: pointer;
+        };
+
+    }
+
+    ul{
+        display: none;
+    }
 </style>
