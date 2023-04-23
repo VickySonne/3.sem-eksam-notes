2fa1dca6cdc2e2b05b568b1101e03b002e6607ab
Cassovich <cassws00@gmail.com>
2023-04-23 19:09:55 +0200

    Styling på table der ikke virker optimalt :((

--- src/components/ProductOverview.vue
+++ src/components/ProductOverview.vue
@@ -3,24 +3,24 @@
 
     <div class="titels-container">
         <table>
-            <tr>
+            <thead>
                 <th>Navn</th>
                 <th>Antal</th>
                 <th>Stk. pris</th>
                 <th>Total</th>
-            </tr>
-            <tr>
-                <td>Centurion Bike</td>
-                <td>1</td>
-                <td>100</td>
-                <td>100</td>
-            </tr>
-            <tr>
-                <td>Trek Bike</td>
-                <td>2</td>
-                <td>100</td>
-                <td>200</td>
-            </tr>
+            </thead>
+                <tbody>
+                    <td>Centurion Bike</td>
+                    <td>1</td>
+                    <td>100</td>
+                    <td>100</td>
+                </tbody>
+                <tbody>
+                    <td>Trek Bike</td>
+                    <td>2</td>
+                    <td>100</td>
+                    <td>200</td>
+                </tbody>
         </table>
         <div class="search-product">
             <div class="search-bar">
@@ -28,6 +28,7 @@
                 <input type="search" placeholder="Søg på varer...">
             </div>
             <button class="add-product">Tilføj vare</button>
+            <p>Tilføj kundeenhed</p>
         </div>
     </div>
 
@@ -57,6 +58,39 @@
         color: white;
         padding: 10px;  
         margin-left: 5px; 
+    }
+
+    th{
+        font-weight: bold;
+    }
+
+    table{
+        width: 100%;
+        td{
+            padding-top: 0.8rem;
+            padding-bottom: 0.8rem;
+        }
+        th{
+            padding-bottom: 0.8rem;
+        }
+    }
+
+    h3{
+        font-weight: 700;
+        font-size: 1.125rem;
+        margin-bottom: 1rem;
+    }
+
+    p{
+        display: flex;
+        align-items: center;
+        padding-left: 1rem;
+        cursor: pointer;
+    }
+
+    i{
+        padding-left: 0.8rem;
+        padding-right: 0.8rem;
     }
 
 </style>
