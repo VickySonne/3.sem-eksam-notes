908f4ca4107c4d9af7f819ca358136e777faf790
CathleenWM <cathleen5068@gmail.com>
2023-04-25 11:48:08 +0200

    Update MainCreateNew.vue

--- src/components/MainCreateNew.vue
+++ src/components/MainCreateNew.vue
@@ -3,6 +3,7 @@ import TodoCatagoriDropdownComp from './TodoCatagoriDropdownComp.vue';
 import ProductOverview from './ProductOverview.vue';
 
 import { createClient } from '@supabase/supabase-js'
+import { equal } from 'assert';
 
 const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY)
 
