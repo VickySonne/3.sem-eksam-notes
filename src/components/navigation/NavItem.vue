a44388bbd84c7039569ebd082741f5e8bd33fc0e
BuzzKill Master <buzzkillmaster@proton.me>
2023-04-27 06:28:52 +0200

    add whitespace to prevent ide warning

--- src/components/navigation/NavItem.vue
+++ src/components/navigation/NavItem.vue
@@ -45,7 +45,7 @@ const isActive = () => {
     <li>
         <router-link :to=route class="nav-item" v-if="!isDisabled" :class="isActive() ? 'active' : ''">
             <div class="icon-container">
-                <font-awesome-icon :icon=icon />
+                <font-awesome-icon :icon=icon/>
             </div>
 
             <p v-if="showLabel">{{ label }}</p>
@@ -63,27 +63,27 @@ const isActive = () => {
 
 <style lang="scss" scoped>
 p {
-    margin-left: 1rem;
-    font-size: 0.875rem;
-    font-weight: 600;
+  margin-left: 1rem;
+  font-size: 0.875rem;
+  font-weight: 600;
 }
 
 .nav-item {
-    align-items: center;
-    border-radius: var(--border-radius);
-    display: flex;
-    padding: 1rem;
+  align-items: center;
+  border-radius: var(--border-radius);
+  display: flex;
+  padding: 1rem;
 
-    &.active,
-    &:hover {
-        background-color: var(--bg-primary);
-        color: var(--text-secondary);
-        cursor: pointer;
-    }
+  &.active,
+  &:hover {
+    background-color: var(--bg-primary);
+    color: var(--text-secondary);
+    cursor: pointer;
+  }
 }
 
 .icon-container {
-    height: 18px;
-    width: 18px;
+  height: 18px;
+  width: 18px;
 }
 </style>
