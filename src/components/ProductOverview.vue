4a385f7d367f328210a1c885a905a206b35bee16
BuzzKill Master <buzzkillmaster@proton.me>
2023-04-27 08:38:58 +0200

    clear ide warnings and improve structure

--- src/components/ProductOverview.vue
+++ src/components/ProductOverview.vue
@@ -1,3 +1,7 @@
+<script setup>
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+</script>
+
 <template>
     <div>
         <h3>Varer</h3>
@@ -5,103 +9,100 @@
         <div class="titels-container">
             <table>
                 <thead>
+                <tr>
                     <th>Navn</th>
                     <th>Antal</th>
                     <th>Stk. pris</th>
                     <th>Total</th>
+                </tr>
                 </thead>
+
                 <tbody>
+                <tr>
                     <td>Centurion Bike</td>
                     <td>1</td>
                     <td>100</td>
                     <td>100</td>
-                </tbody>
-                <tbody>
+                </tr>
+
+                <tr>
                     <td>Trek Bike</td>
                     <td>2</td>
                     <td>100</td>
                     <td>200</td>
+                </tr>
                 </tbody>
             </table>
+
             <div class="search-product">
                 <div class="search-bar">
-                    <i>i</i>
+                    <font-awesome-icon icon="magnifying-glass"/>
                     <input type="search" placeholder="Søg på varer...">
                 </div>
+
                 <button class="add-product">Tilføj vare</button>
                 <p>Tilføj kundeenhed</p>
             </div>
         </div>
     </div>
-
 </template>
 
 <style lang="scss" scoped>
-    .titels-container{
-        background-color: rgb(229 231 235);
-    }
+.titels-container {
+  background-color: rgb(229 231 235);
+}
 
-    .search-product{
-        display: flex;
-        flex-direction: row;
-        gap: 1rem;
-        padding: 1rem;
+.search-product {
+  display: flex;
+  gap: 1rem;
+  padding: 1rem;
 
-        p{
-            display: flex;
-            align-items: center;
-            cursor: pointer;
-        }
+  p {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
 
-        p:hover{
-            text-decoration: underline;
-        }
-    }
+      &:hover {
+          text-decoration: underline;
+      }
+  }
+}
 
-    .search-bar{
-        background-color: white;
-        display: flex;
-        align-items: center;
-        gap: 1rem;
-        padding: 1rem 3rem;
-        position: relative;
+.search-bar {
+  align-items: center;
+  background-color: white;
+  display: flex;
+  gap: 1rem;
+  padding: 1rem;
+}
 
-        i{
-            position: absolute;
-            left: 1rem;
-        }
-    }
+.add-product {
+  cursor: pointer;
+  background-color: #1E293B;
+  color: white;
+  padding: 1rem;
 
-    .add-product{
-        cursor: pointer; 
-        background-color: #1E293B;   
-        color: white;
-        padding: 1rem; 
-    }
-
-    .add-product:hover{
+    &:hover {
         background-color: rgb(71 85 105);
     }
+}
 
-    th{
-        font-weight: bold;
-    }
+table {
+  width: 100%;
 
-    table{
-        width: 100%;
-        th{
-            padding: 1rem;
-        }
-        td{
-            padding: 0.5rem 1rem;
-        }
-    }
+  th {
+      font-weight: bold;
+    padding: 1rem;
+  }
 
-    h3{
-        font-weight: 700;
-        font-size: 1.125rem;
-        margin-bottom: 1rem;
-    }
+  td {
+    padding: 0.5rem 1rem;
+  }
+}
 
-
+h3 {
+  font-weight: 700;
+  font-size: 1.125rem;
+  margin-bottom: 1rem;
+}
 </style>
