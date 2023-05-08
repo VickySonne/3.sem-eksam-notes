06c0757eb0d891ea6c3b7f1bedc46eb20132bf1f
Christian Pedersen <crpe44102@edu.ucl.dk>
2023-05-08 13:58:25 +0200

    remove price division

--- src/utilities/formatPrice.js
+++ src/utilities/formatPrice.js
@@ -2,7 +2,7 @@ const formatPrice = (price) => {
     return new Intl.NumberFormat('da-DK', {
         style: 'currency',
         currency: 'DKK',
-    }).format(price / 100)
+    }).format(price)
 }
 
 export default formatPrice
