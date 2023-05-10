fbd2817bfb539c8cde13b82315aab6fa98b11112
CathleenWM <cathleen5068@gmail.com>
2023-05-10 14:19:44 +0200

    Setting up CTA button components

---
+++ src/components/new components/CreateNewCaseButton.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <font-awesome-icon icon="plus"/>
+        <p>Opret Ny Sag</p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+div {
+  align-items: center;
+  display: flex;
+  gap: var(--default-gap);
+  border-radius: var(--border-radius);
+  background-color: var(--CTA-primary);
+  color: var(--text-secondary);
+  padding: var(--default-padding);
+  cursor: pointer;
+}
+
+div:hover {
+  background-color: var(--CTA-secondary-hover);
+}
+
+@media screen and (min-width: var(--tablet-query)) {
+    
+}
+
+@media screen and (min-width: var(--desktop-query)) {
+    
+}
+</style>
