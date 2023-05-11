21141a961188edcbc1e7b6895f18981baddee30f
VickySonne <vst1996@live.dk>
2023-05-11 09:07:20 +0200

    refactor searchbar comp

--- src/components/new components/SearchBar.vue
+++ src/components/new components/SearchBar.vue
@@ -1,9 +1,30 @@
-<script></script>
+<script>
+    import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+</script>
 
 <template>
-    
+    <div>
+        <font-awesome-icon icon="magnifying-glass"/>
+        <input type="text" :placeholder="{placeholder}" v-model="searchRef">
+        <font-awesome-icon icon="times"/>
+    </div>
 </template>
 
 <style lang="scss" scoped>
+    div{
+    align-items: center;
+    background-color: var(--bg-light);
+    border-radius: var(--border-radius);
+    display: flex;
+    gap: var(--default-gap);
+    padding-inline: var(--default-padding);
 
+    input {
+      padding-block: var(--default-padding);
+    }
+
+    &last-child {
+      cursor: pointer;
+    }
+  }
 </style>
