4931d813a672aae6de5a4c9a9926acc1b43dcd03
VickySonne <vst1996@live.dk>
2023-05-11 12:51:38 +0200

    Create TableBody comp and ProductOverviewProductRow Component

---
+++ src/components/new components/ProductOverviewProductRow.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+const props = defineProps(['data']);
+</script>
+
+<template>
+    <tr>
+        <td>{{ product.name }}</td>
+
+        <td class="product-count">
+            <span>{{ product.count }}</span>
+
+            <div v-if="products.length" @click="decrementProduct(product)">
+                <font-awesome-icon icon="caret-down"/>
+            </div>
+
+            <div v-if="products.length" @click="incrementProduct(product)">
+                <font-awesome-icon icon="caret-up"/>
+            </div>
+
+            <div v-if="products.length" @click="removeProduct(product)">
+                <font-awesome-icon icon="times"/>
+            </div>
+        </td>
+
+        <td>{{ product.sell_price }}</td>
+        <td>{{ product.sell_price * product.count }}</td>
+</tr>
+</template>
