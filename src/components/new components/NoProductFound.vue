b82c785ed5918000520726539524561cef1446aa
CathleenWM <cathleen5068@gmail.com>
2023-05-11 11:10:24 +0200

    Update NoProductFound.vue

--- src/components/new components/NoProductFound.vue
+++ src/components/new components/NoProductFound.vue
@@ -3,9 +3,3 @@
         <p>Ingen varer fundet. <span>Klik for at oprette en ny vare.</span></p>
     </div>
 </template>
-
-<style>
-/* .noProduct:hover {
-    color: var(--bg-dark);
-} */
-</style>
