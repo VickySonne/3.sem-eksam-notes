a3ddeb65e12589ca0df2788ed55569c63888d1cd
CathleenWM <cathleen5068@gmail.com>
2023-05-11 10:38:56 +0200

    Updating

--- src/components/new components/FoundNoCustomer.vue
+++ src/components/new components/FoundNoCustomer.vue
@@ -1,6 +1,5 @@
 <template>
-        <div @click="comingSoonDialogue"
-                v-if="!customerOptions.filter(c => recursiveObjectSearch(c, customerSearchRef)).length">
+        <div>
             <p>Ingen kunder fundet. <span>Klik for at oprette en ny kunde.</span></p>
         </div>
 </template>
--- src/pages/CreateNewCase.vue
+++ src/pages/CreateNewCase.vue
@@ -339,8 +339,8 @@ const deleteCase = () => {
                       <FoundNoCustomer/>
                         <!-- <div @click="comingSoonDialogue"
                              v-if="!customerOptions.filter(c => recursiveObjectSearch(c, customerSearchRef)).length">
-                            <p>Ingen kunder fundet. <span>Klik for at oprette en ny kunde.</span></p> -->
-                        </div>
+                            <p>Ingen kunder fundet. <span>Klik for at oprette en ny kunde.</span></p> 
+                        </div>-->
 
                         <div v-for="customer in customerOptions.filter(c => recursiveObjectSearch(c, customerSearchRef))"
                              @click="() => {
