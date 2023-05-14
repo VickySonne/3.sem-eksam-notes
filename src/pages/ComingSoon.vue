d326a357a13e7557088c3f1376325c2f73b4ef45
BuzzKill Master <buzzkillmaster@proton.me>
2023-05-14 09:06:34 +0200

    minor refactoring of coming soon page

--- src/pages/ComingSoon.vue
+++ src/pages/ComingSoon.vue
@@ -1,3 +1,21 @@
 <template>
-    <h1>Coming Soon</h1>
+    <section>
+        <h2>Kommer Snart</h2>
+    </section>
 </template>
+
+<style scoped>
+section {
+    align-items: center;
+    display: flex;
+    height: 100%;
+    justify-content: center;
+}
+
+h2 {
+    font-size: 2rem;
+    font-weight: var(--text-heavy);
+}
+</style>
+<script setup>
+</script>
