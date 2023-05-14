64ab59c287f8781725e320e5dd5294c34831e5c7
BuzzKill Master <buzzkillmaster@proton.me>
2023-05-14 12:08:46 +0200

    fetch and display customer information when viewing a case

--- src/pages/cases/components/read/CaseCustomer.vue
+++ src/pages/cases/components/read/CaseCustomer.vue
@@ -1,6 +1,13 @@
 <script setup>
 import SectionContainer from "@/components/layout/section/SectionContainer.vue";
 import SectionHeader from "@/components/layout/section/SectionHeader.vue";
+import supabase from "@/database";
+import router from "@/router";
+
+const { data } = await supabase.from("cases")
+    .select("id, customer(name, email, phone, address, city, zipcode)")
+    .eq("id", router.currentRoute.value.params.id)
+    .single();
 </script>
 
 <template>
@@ -9,6 +16,15 @@ import SectionHeader from "@/components/layout/section/SectionHeader.vue";
             <SectionHeader title="Kunde" />
         </template>
 
-        <h1>Customer Information</h1>
+        <ul>
+            <li v-for="(value, key) in data.customer" :key="key">{{ value }}</li>
+        </ul>
     </SectionContainer>
 </template>
+
+<style lang="scss" scoped>
+ul {
+    display: grid;
+    gap: var(--half-padding)
+}
+</style>
