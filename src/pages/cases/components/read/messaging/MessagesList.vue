b0ac0855c9599b24a732a8c2076e857488dce2df
BuzzKill Master <buzzkillmaster@proton.me>
2023-05-15 06:02:01 +0200

    only show messages list if there are any messages

--- src/pages/cases/components/read/messaging/MessagesList.vue
+++ src/pages/cases/components/read/messaging/MessagesList.vue
@@ -19,7 +19,7 @@ onBeforeUnmount(() => {
         <h3>Ingen beskeder fundet.</h3>
     </div>
 
-    <ul>
+    <ul v-if="messages.length">
         <Message v-for="message in messages" :message=message :key="message.id" />
     </ul>
 </template>
