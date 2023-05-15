0e13c34054572289606b991afa802fe4f8d8ae98
BuzzKill Master <buzzkillmaster@proton.me>
2023-05-15 05:58:51 +0200

    reintroduce back button

---
+++ src/components/layout/navigation/BackButton.vue
@@ -0,0 +1,34 @@
+<script setup>
+import router from "@/router";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+
+defineProps({
+    destination: {
+        type: String,
+        required: false,
+        default: '/',
+    }
+})
+</script>
+
+<template>
+    <div @click="router.push({ path: destination })">
+        <font-awesome-icon icon="arrow-left"/>
+
+        <p>
+            <slot></slot>
+        </p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+div {
+  align-items: center;
+  cursor: pointer;
+  display: flex;
+  font-weight: 600;
+  gap: 1rem;
+  padding-bottom: 2rem;
+  width: fit-content;
+}
+</style>
--- src/pages/cases/ReadCase.vue
+++ src/pages/cases/ReadCase.vue
@@ -5,9 +5,12 @@ import CaseCustomer from "@/pages/cases/components/read/CaseCustomer.vue";
 import CaseMessaging from "@/pages/cases/components/read/CaseMessaging.vue";
 import CaseProducts from "@/pages/cases/components/read/CaseProducts.vue";
 import CaseTasks from "@/pages/cases/components/read/CaseTasks.vue";
+import BackButton from "@/components/layout/navigation/BackButton.vue";
 </script>
 
 <template>
+    <BackButton>Tilbage til oversigt</BackButton>
+
     <BaseGrid>
         <template #left-column>
             <CaseDetails />
