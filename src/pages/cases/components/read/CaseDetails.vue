192605aac9cd348b16dafd47373d42d76fc9fddb
Cassovich <105642012+Cassovich@users.noreply.github.com>
2023-05-15 12:35:34 +0200

    show case details + modify reducers

--- src/pages/cases/components/read/CaseDetails.vue
+++ src/pages/cases/components/read/CaseDetails.vue
@@ -1,6 +1,20 @@
 <script setup>
 import SectionContainer from "@/components/layout/section/SectionContainer.vue";
 import SectionHeader from "@/components/layout/section/SectionHeader.vue";
+import detailsReducer from "@/pages/cases/components/read/reducers/detailsReducer.js"
+import { onUnmounted, ref } from "vue";
+
+const isLoading = ref(true)
+
+const details = detailsReducer.details
+
+detailsReducer.fetchDetails().then(() => {
+    isLoading.value = false
+})
+
+onUnmounted(() => {
+    detailsReducer.flush()
+})
 </script>
 
 <template>
@@ -9,6 +23,26 @@ import SectionHeader from "@/components/layout/section/SectionHeader.vue";
             <SectionHeader title="Detaljer" />
         </template>
 
-      <h1>Case Details</h1>
+        <div v-if="isLoading" class="loader"></div>
+
+        <div v-if="!isLoading">
+            <p><span>Ansvarlig:</span> {{ details.responsible_employee.name }}</p>
+            <p><span>Status:</span> {{ details.status.name }}</p>
+            <p><span>Afhentningstidspunkt:</span> {{ new Date(details.pickup).toLocaleDateString() }}</p>
+            <p><span>Aftalt pris:</span> {{ details.negotiated_price }}</p>
+            <p><span>Oprettet af:</span> {{ details.created_by.name }}</p>
+            <p><span>Oprettet d.</span> {{ new Date(details.created_at).toLocaleDateString() }}</p>
+            <p><span>Beskrivelse:</span> {{ details.description }}</p>
+        </div>
     </SectionContainer>
 </template>
+
+<style lang="scss" scoped>
+    span{
+        font-weight: bold;
+    }
+
+    p{
+        padding-block: var(--half-padding);
+    }
+</style>
