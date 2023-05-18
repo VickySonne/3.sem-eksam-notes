6822b31e38eba6d79e91ed8aad2ae51387533d3f
Cassovich <105642012+Cassovich@users.noreply.github.com>
2023-05-18 11:46:45 +0200

    added scope to page title styling

--- src/components/layout/PageTitle.vue
+++ src/components/layout/PageTitle.vue
@@ -11,10 +11,10 @@
     <h1>{{ title }}</h1>
 </template>
 
-<style lang="scss">
-h1{
+<style lang="scss" scoped>
+h1 {
     font-weight: var(--text-heavy);
     font-size: 2rem;
     padding-block: 1rem;
-    }
+}
 </style>
