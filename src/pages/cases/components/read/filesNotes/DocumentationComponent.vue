5c86566923a6219dc5320d07467df30d4521a178
VickySonne <vst1996@live.dk>
2023-05-20 16:16:30 +0200

    Update DocumentationComponent.vue

---
+++ src/pages/cases/components/read/filesNotes/DocumentationComponent.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+component to loop through with dokumentation
+
+v-else (no files)
+
+<div>
+
+</div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
