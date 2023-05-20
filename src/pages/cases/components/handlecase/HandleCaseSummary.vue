78677a3e7ce3e0a44cf665ed274e21be6d104d26
BuzzKill Master <buzzkillmaster@proton.me>
2023-05-20 11:36:12 +0200

    prepare create case structure - some modifications still needed

--- src/pages/cases/components/handlecase/HandleCaseSummary.vue
+++ src/pages/cases/components/handlecase/HandleCaseSummary.vue
@@ -1,3 +1,14 @@
+<script setup>
+import SectionContainer from "@/components/layout/section/SectionContainer.vue";
+import SectionHeader from "@/components/layout/section/SectionHeader.vue";
+</script>
+
 <template>
-    her
+    <SectionContainer>
+        <template #header>
+            <SectionHeader title="Opsummering" />
+        </template>
+
+        <!-- stuff goes here -->
+    </SectionContainer>
 </template>
