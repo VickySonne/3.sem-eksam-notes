24b05814ecd5ca278113116450d208392ec68492
BuzzKill Master <buzzkillmaster@proton.me>
2023-05-23 13:20:34 +0200

    fix description text input not updating case description

--- src/pages/cases/components/handlecase/details/TextareaInput.vue
+++ src/pages/cases/components/handlecase/details/TextareaInput.vue
@@ -1,10 +1,19 @@
-<Script setup>
-
-</Script>
+<script setup>
+defineProps({
+    value: {
+        type: String,
+        required: true
+    },
+    callback: {
+        type: Function,
+        required: true
+    }
+})
+</script>
 
 <template>
-    <textarea name="" id="" cols="30"
-            rows="10" placeholder="Indtast beskrivelse af her..."></textarea>
+    <textarea :value="value" name="" id="" cols="30"
+            rows="10" placeholder="Indtast beskrivelse af her..." @input="callback($event)"></textarea>
 </template>
 
 <style lang="scss" scoped>
@@ -14,5 +23,4 @@
         padding: var(--default-padding);
         width: 100%;
     }
-    
 </style>
